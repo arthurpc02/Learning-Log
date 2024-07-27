cdbc9bc24076a1319a69c680a7db1b5a30dcf297
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2024-07-27 22:02:21 +0000

    added a page to add new entries, but it's incomplete.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,10 +7,12 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Home page
     path('', views.index, name='index'),
-    # Page that show all topics.
+    # Page that show all topics:
     path('topics/', views.topics, name='topics'),
-    # Detail page for a single topic.
+    # Detail page for a single topic:
     path('topics/<int:topic_id>/', views.topic, name='topic'),
-    # Page for adding a new topic
+    # Page for adding a new topic:
     path('new_topic/', views.new_topic, name='new_topic'),
+    # Page for adding a new entry:
+    path('new_entry/', views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """The home page for Learning Log."""
@@ -34,3 +34,19 @@ def new_topic(request):
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request):
+    """Add a new entry"""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = EntryForm()
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+        
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
