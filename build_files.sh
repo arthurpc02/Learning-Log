461ab804d723b1cb1ecb236ad2fe13d81a06fba2
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2024-08-24 19:47:31 +0000

    skips migrattions because sqlite is not supported anyway

--- build_files.sh
+++ build_files.sh
@@ -7,7 +7,7 @@ echo "Installing dependencies:"
 python3 -m pip install -r requirements.txt
 
 echo "Migrating the database:"
-python3 manage.py migrate --noinput
+# python3 manage.py migrate --noinput
 
 echo "Collecting static files:"
 python3 manage.py collectstatic --noinput
