d6c86651f1776bf1f7432b54dc58f071ff192f34
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2024-08-24 19:25:25 +0000

    build_files.sh now installs the dependencies before starting.

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Migrating the database..."
+echo "Installing dependencies:"
+python3 -m pip install -r requirements.txt
+
+echo "Migrating the database:"
 python3 manage.py migrate --noinput
 
-echo "Collecting static files..."
+echo "Collecting static files:"
 python3 manage.py collectstatic --noinput
