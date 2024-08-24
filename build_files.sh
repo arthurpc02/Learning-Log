9d507b8fe375c7acbf095985851db2b9ca697539
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2024-08-24 19:49:24 +0000

    also skipping collectstatic because it also has to do with sqlite.

--- build_files.sh
+++ build_files.sh
@@ -6,8 +6,8 @@ python3 --version
 echo "Installing dependencies:"
 python3 -m pip install -r requirements.txt
 
-echo "Migrating the database:"
+# echo "Migrating the database:"
 # python3 manage.py migrate --noinput
 
-echo "Collecting static files:"
-python3 manage.py collectstatic --noinput
+# echo "Collecting static files:"
+# python3 manage.py collectstatic --noinput
