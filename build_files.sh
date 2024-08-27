f3d2acbd8f2bb6705398f7d77db14c05cdb048fc
Arthur P. Côrtes <35460423+arthurpc02@users.noreply.github.com>
2024-08-27 21:41:54 +0000

    forgot to add migrate command to build_files.sh.

--- build_files.sh
+++ build_files.sh
@@ -6,11 +6,12 @@ python3 --version
 echo "Installing dependencies:"
 python3 -m pip install -r requirements.txt
 
+echo "Migrating the database:"
+python3 manage.py migrate
+
 echo "Run the server:"
 python3 manage.py runserver
 
-# echo "Migrating the database:"
-# python3 manage.py migrate --noinput
 
 # echo "Collecting static files:"
 # python3 manage.py collectstatic --noinput
